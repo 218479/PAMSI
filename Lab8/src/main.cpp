2f0388cb3091d90765d723d125d937c9d5c2e259
218479 <218479@student.pwr.edu.pl>
2016-05-31 00:07:33 +0200

    Dodano Branch&Bound, sprawozdanie oraz proprowadzono czastkowe testy

--- Lab8/src/main.cpp
+++ Lab8/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include "Graf.hh"
+#include "Test.hh"
 
 
 using namespace std;
@@ -8,6 +9,24 @@ using namespace std;
 int main()
 {
 
+ //   Test Nowy;
+ //   Nowy.wykonaj();
+    Graf Nowy(5);
 
+    Nowy.ZrobPolaczenie(0,1,3);
+    Nowy.ZrobPolaczenie(0,2,5);
+
+    Nowy.ZrobPolaczenie(1,2,3);
+    Nowy.ZrobPolaczenie(1,3,7);
+    Nowy.ZrobPolaczenie(1,4,3);
+
+    Nowy.ZrobPolaczenie(3,4,1);
+
+    Nowy.CzyPolaczone(0,1);
+    Nowy.CzyPolaczone(1,4);
+
+    cout<<Nowy.BranchAndBoundWithExtendedList(4);
+    cout<<"\n\n\n\n\n";
+    cout<<Nowy.BranchAndBound(4);
     return 0;
 }
